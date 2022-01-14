a2f97c1ebbc9611290b01f12b84061e631c6e58d
Ebenezer Nikabou <nikaboue10@gmail.com>
2022-01-14 11:37:40 +0000

    Initial commit

---
+++ src/main/java/com/ebenjs/openstacktrace/OpenstacktraceApplication.java
@@ -0,0 +1,13 @@
+package com.ebenjs.openstacktrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenstacktraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenstacktraceApplication.class, args);
+	}
+
+}
